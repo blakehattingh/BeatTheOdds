e987c51adacf20179cbed87b72364e8b6479a0f8
blakehattingh <blake.hattingh123@gmail.com>
2021-08-18 18:32:42 +1200

    more computing p code

--- ComputingP/CalculatingP.py
+++ ComputingP/CalculatingP.py
@@ -3,9 +3,9 @@ from typing import Match
 from pandas.core.base import DataError
 import os, sys
 relativePath = os.path.abspath('')
-sys.path.append(relativePath + '\\MarkovModel')
+#sys.path.append(relativePath + '\\MarkovModel')
 sys.path.append(relativePath + '\\DataExtraction')
-from FirstImplementation import MarkovModelFirstImplementation
+#from FirstImplementation import MarkovModelFirstImplementation
 from TestSetCollector import *
 from DataCollector import *
 import pandas as pd
@@ -47,12 +47,9 @@ def ObjectiveMetricSetScore(AllSetScoreDist, SetScores):
 
 def ObjectiveMatchScoreDist(MatchScoreDist, MatchScore):
     x = 10
-<<<<<<< HEAD
 
 def InterpolateDists(Pa, Pb):
     x = 10
-=======
->>>>>>> d0d03ab6ca45d8093cb79f96209f37de77a7938a
     
 def try_parsing_date(text):
     for fmt in ('%d/%m/%Y', '%d-%m-%Y', '%Y-%m-%d'):
@@ -69,24 +66,25 @@ def EvalEquation(age):
 
     for year in sampledMatchesByYears:
         for match in year:
-            dateOfMatch = datetime.strptime(match[3],'%Y-%m-%d')
+            dateOfMatch = match[3]
             startOfDataCollection = dateOfMatch - ageGap
             p1vP2,p1vCO,p2vCO,COIds = getSPWData(match, startOfDataCollection)
             Pa,Pb = CalcPEquation1(match, p1vP2, p1vCO, p2vCO, COIds, 0.3,0.3)
+            print(Pa,Pb)
 
     
 
     # Evaluate the objective metrics:
 
     # Match Outcome:
-    Prop = ObjectiveMatchScoreDist(MatchScoreDist, MatchData['MatchOutcome'])
+    #Prop = ObjectiveMatchScoreDist(MatchScoreDist, MatchData['MatchOutcome'])
     
     # Set Scores:
-    Points = ObjectiveMetricSetScore(AllSetScoresDist, MatchData['SetScores'])
+    #Points = ObjectiveMetricSetScore(AllSetScoresDist, MatchData['SetScores'])
 
     # Return Objective:
-    ObjectiveVals = {'MatchOutcome': Prop, 'SetScores': Points, 'MatchScore': 0}
-    return ObjectiveVals
+    #ObjectiveVals = {'MatchOutcome': Prop, 'SetScores': Points, 'MatchScore': 0}
+    #return ObjectiveVals
 
 def CalcPEquation1(MatchData,PrevMatches,PrevMatchesCommA,PrevMatchesCommB,CommonOpps,Lamda,SurfaceParameter):
     # This function takes in a match, extracts who is playing, when the match is/was played, and what surface it is/was played on
@@ -130,7 +128,7 @@ def CalcPEquation1(MatchData,PrevMatches,PrevMatchesCommA,PrevMatchesCommB,Commo
     Pb = (1  - Lamda) * spwBA + Lamda * spwBC
 
     print('Pa:', Pa, 'Pb:', Pb)
-    return [Pa, Pb]
+    return Pa, Pb
 
 def ComputeSPW(PlayerA, PlayerB, PrevMatches, SurfaceParameter, Date, Surface):
     # Inputs:
@@ -151,7 +149,7 @@ def ComputeSPW(PlayerA, PlayerB, PrevMatches, SurfaceParameter, Date, Surface):
     PlayerBServePointsWonNS = 0
     for match in range(len(PrevMatches)):
         # Check when the match was played:
-        MatchDate = datetime.strptime(PrevMatches[match][3], '%Y-%m-%d')
+        MatchDate = PrevMatches[match][3]
 
         if ((Date - MatchDate).days > 0):
             if (PrevMatches[match][4] == Surface):
@@ -178,6 +176,8 @@ def ComputeSPW(PlayerA, PlayerB, PrevMatches, SurfaceParameter, Date, Surface):
                     PlayerBServePointsWonNS += (PrevMatches[match][44] + PrevMatches[match][45])
             
     # Compute the proportion of service points won:
+    print(PlayerAServePointsSurface, PlayerAServePointsNS)
+    print(len(PrevMatches))
     PlayerAServiceProp = (1 - SurfaceParameter) * (PlayerAServePointsWonSurface / PlayerAServePointsSurface) + SurfaceParameter * (PlayerAServePointsWonNS / PlayerAServePointsNS)
     PlayerBServiceProp = (1 - SurfaceParameter) * (PlayerBServePointsWonSurface / PlayerBServePointsSurface) + SurfaceParameter * (PlayerBServePointsWonNS / PlayerBServePointsNS)
 
@@ -203,7 +203,7 @@ def ComputeSPWCommon(PlayerA, PrevMatchesCommOpps, CommonOpps, SurfaceParameter,
     PlayerAReturnPointsWonNS = np.zeros(len(CommonOpps), dtype = int)
     for match in range(len(PrevMatchesCommOpps)):
         # Check when the match was played:
-        MatchDate = datetime.strptime(PrevMatchesCommOpps[match][3], '%Y-%m-%d')
+        MatchDate = PrevMatchesCommOpps[match][3]
 
         if ((Date - MatchDate).days > 0):
             if (Surface == PrevMatchesCommOpps[match][4]):
@@ -252,37 +252,7 @@ def ComputeSPWCommon(PlayerA, PrevMatchesCommOpps, CommonOpps, SurfaceParameter,
     return [OverallSPWCommOpps, OverallRPWCommOpps]
 
 def main():
-    # Create match data:
-    MatchData = {'MatchOne': {'MatchOutcome': 2, 'SetScores': [[4,6],[6,4]], 'MatchScore': 3, 'Date': '19/08/2018', 'Surface': 'H'}, 
-    'MatchTwo': {'MatchOutcome': 2, 'SetScores': [[6,7],[7,5],[6,7]], 'MatchScore': 4, 'Date': '03/11/2018', 'Surface': 'H'}}
-
-    # Common Opponents:
-    CommOpps = [5216,3898,4913,5663,3900,4606,4742,5442,3786,3602,4053,4789,3720,2318,4541,3206,3344,3163,4868,3017,26577,2450,5201,4526,
-    5220,27834,5763,3096,3084,6219,4752,4659,4544,644,2148,6409,2257,3656,5670,4570,5349,3888,4338,4467,3454,3103,4728,3498,6418,3507,4311,5070,
-    5630,3990,3333,5918,3285,3835,4416,3484,2783,3632,4664,3781,6387,3852,3428,5543,3823,5131,5922,4585,4259,5515,4994,3292,5016,3917,
-    3813,4068,4035,3758,5166,5159,6401,4122,4098,2179,4022,5210,4470,2839,4269,4229,4675,3582,11547,4019,3843,3808,3970,5055,4716,4214,
-    3694,2845,2565,2720,6029,5565,5303,3909,34553,5655,4198,5324,4677,32067,3566,6044,4291,5088,6407,5034,3598,4794,5801,4180,4337,
-    5978,4894,5902,4385,5539,4596,3722,3429,5057,5231,26006,4533,4654,4252,5438,6284,6364,3908,5986,2967,4619,3812,6031,5718,4499,3752,
-    5046,26413,4921,4914,3794,3565,4225,5571,6057,33214,3893,4326,4879,5793,4592,11176,36221,5370,26010,5375,4493,3503,3110,5636,4331,
-    3181,6214,4591,4974]
-
-    # Store Objective Points:
-    Objective = {'MatchOutcome': 0, 'SetScores': 0, 'MatchScore': 0}
-
-    # Run the model using equation 1 for the matches:
-    for match in MatchData.values():
-        # Compute the P values:
-        [Pa, Pb] = CalcPEquation1(3819, 4920, CommOpps, 'SQLData/fedVsDjok.csv', 'SQLData/fedVsComOppDjok.csv', 'SQLData/DjokVsComOppfed.csv',
-        match['Date'], match['Surface'], 0.5, 12, 0.5)
-
-        # Evaluate how well the model did:
-        Obj = EvalEquation(Pa, Pb, match)
-
-        # Record the objective scores:
-        for Metric in Obj:
-            Objective[Obj] += Obj[Metric]
-
-    print(Objective)
+    EvalEquation(8)
 
 
 if __name__ == "__main__":
